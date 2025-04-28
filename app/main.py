07645361b5a8210572ddfbf9da2053dc59057dd4
supercoder382001 <63428439+supercoder382001@users.noreply.github.com>
2025-04-28 06:32:35 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
-from app.routers import mail, map, payment
+from fastapi.middleware.cors import CORSMiddleware
+from app.routers import mail, map, payment,phonepe,phonepepy
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Or specify allowed origins like ["https://yourdomain.com"]
+    allow_credentials=True,
+    allow_methods=["*"],  # Or specify like ["GET", "POST"]
+    allow_headers=["*"],
+)
 # Include routers
 app.include_router(mail.router, prefix="/api", tags=["Mail"])
 app.include_router(map.router, prefix="/api", tags=["Map"])
-app.include_router(payment.router, prefix="/api", tags=["Payment"])
+# app.include_router(payment.router, prefix="/api", tags=["Payment"])
+# app.include_router(phonepe.router, prefix="/api", tags=["Phonepe"])
+# app.include_router(phonepepy.router, prefix="/api", tags=["Phonepepayment"])
 
 @app.get("/")
 async def root():
